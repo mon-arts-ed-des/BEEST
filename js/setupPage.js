8fc54fb3f78dd5827d2c3ccf9e1b9c9148807a1d
Timothy Scholl <Timbolini@dyn-49-127-66-149.its.monash.edu.au>
2020-11-19 13:41:11 +1100

    reinstate setupPage.js deleted in my revert break

---
+++ js/setupPage.js
@@ -0,0 +1,54 @@
+function activeNav() {
+	var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+	 $(".nav-link").each(function(){
+		  if($(this).attr("href") == pgurl || $(this).attr("href") == '' ){
+		  $(this).addClass("active");
+		  $(this).removeClass("text-white");
+		  $(this).append('<span class="sr-only">(current)</span>');
+		  $(this).closest('.nav-item').css({"background": "#004DAE"});
+	 };
+	});
+};
+function setupNav(){
+	$('#beestNav').load('common/beestnav.html',"",function(){
+		activeNav();
+	});
+	$('#beestFooter').load('common/beestfooter.html');
+	
+};
+function setupToolTips(longTime,shortTime){
+	if (longTime==undefined){
+		longTime = 7000
+	}
+	if (shortTime == undefined){
+		shortTime = 2500
+	}
+	
+	longShowToolTip(longTime)
+	shortHideToolTipOnClick('copyCodeBtn',shortTime)
+	shortHideToolTipOnClick('toggleHelp',shortTime)
+}
+
+function longShowToolTip(longTime) {
+	$(document).on('shown.bs.tooltip', function (e) {
+		setTimeout(function () {
+			$(e.target).tooltip('hide');
+		}, longTime);
+	});
+}
+
+function shortHideToolTipOnClick(id,shortTime){
+	$('#'+id).on('click', function (e) {
+		setTimeout(function () {
+			$(e.target).tooltip('hide');
+		}, shortTime);
+	});
+}
+
+function setupGoogleAnalytics(){
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-152392632-3');
+}
