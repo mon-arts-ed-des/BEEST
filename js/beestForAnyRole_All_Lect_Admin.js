96299c8de9f1114886ff8e2f5c7c0e97b8335834
Phillip Abramson <Phillip.Abramson@monash.edu>
2021-02-05 15:43:31 +1100

    switching to a faster cipher to (hopefully) speed up loading the button

--- js/beestForAnyRole_All_Lect_Admin.js
+++ js/beestForAnyRole_All_Lect_Admin.js
@@ -5,10 +5,10 @@ const match_lect = /Edit settings/;
 const cog_presence_regex = /Recycle bin/;
 const COLLAPSED_MODE = "collapsed"
 const KEY_COLLAPSE_BEEST_EDIT_RAW = "wasSeen"
-const KEY_COLLAPSE_BEEST_EDIT = sha256(KEY_COLLAPSE_BEEST_EDIT_RAW)
+const KEY_COLLAPSE_BEEST_EDIT = encodehash(KEY_COLLAPSE_BEEST_EDIT_RAW)
 
 const key_current_role_raw = "CURRENT_ROLE"
-const key_current_role = sha256(key_current_role_raw)
+const key_current_role = encodehash(key_current_role_raw)
 var match = [
 	[match_admin.toString(),"admin"],
 	[match_lect.toString(),"lect"]
@@ -82,7 +82,7 @@ function setup_beest(MODE,visibilityMethod){
 	
 	if((cog_present)&&(correct_role)){
 		//if at any point cog present and can find the required text...
-		role = sha256(regex_to_role[mode])
+		role = encodehash(regex_to_role[mode])
 		
 		/*POSSIBLE DANGER: someone with cog access could change in the page the role by modifying localStorage in their browser directly
 		SOLUTION: encrypt both the localStorage key and value to make it practically impossible to change without knowledge of this code
@@ -98,7 +98,7 @@ function setup_beest(MODE,visibilityMethod){
 	}
 	else if ((cog_present)&&(!(correct_role))){
 		//cog present but not correct role
-		role = sha256("invalid")
+		role = encodehash("invalid")
 		save_to_local(key_current_role,role)
 		//override localStorage with 'invalid' option
 	}
@@ -108,7 +108,7 @@ function setup_beest(MODE,visibilityMethod){
 			role = retrieve_from_local(key_current_role) //should already be encrypted in SHA256
 		
 			//to decide whether to show or hide beest
-			if (sha256(regex_to_role[mode])===role){
+			if (encodehash(regex_to_role[mode])===role){
 				//if the role (encrypted) matches the role for the lookup (encrypted) then make visible
 				if (!(beest_icon_visible)){
 					make_beest_visible(visibilityMethod);
@@ -199,101 +199,59 @@ function make_beest_visible(visibilityMethod){
 	}
 }
 
+function encodehash(input){
+	return murmurhash2_32_gc(input, 0xFFECD ).toString(16)
+}
 
-/*SHA256 function to encrypt sensitive data (make it hard for someone to dynamically change role in localStorage) -- credit: https://geraintluff.github.io/sha256/*/
-function sha256(ascii) {
-    function rightRotate(value, amount) {
-        return (value>>>amount) | (value<<(32 - amount));
-    };
-    
-    var mathPow = Math.pow;
-    var maxWord = mathPow(2, 32);
-    var lengthProperty = 'length'
-    var i, j; // Used as a counter across the whole file
-    var result = ''
+/*function to encrypt sensitive data (make it hard for someone to dynamically change role in localStorage)*/
 
-    var words = [];
-    var asciiBitLength = ascii[lengthProperty]*8;
-    
-    //* caching results is optional - remove/add slash from front of this line to toggle
-    // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
-    // (we actually calculate the first 64, but extra values are just ignored)
-    var hash = sha256.h = sha256.h || [];
-    // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
-    var k = sha256.k = sha256.k || [];
-    var primeCounter = k[lengthProperty];
-    /*/
-    var hash = [], k = [];
-    var primeCounter = 0;
-    //*/
+/**
+ * JS Implementation of MurmurHash2
+ * 
+ * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
+ * @see http://github.com/garycourt/murmurhash-js
+ * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
+ * @see http://sites.google.com/site/murmurhash/
+ * 
+ * @param {string} str ASCII only
+ * @param {number} seed Positive integer only
+ * @return {number} 32-bit positive integer hash
+ */
 
-    var isComposite = {};
-    for (var candidate = 2; primeCounter < 64; candidate++) {
-        if (!isComposite[candidate]) {
-            for (i = 0; i < 313; i += candidate) {
-                isComposite[i] = candidate;
-            }
-            hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
-            k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
-        }
-    }
+function murmurhash2_32_gc(str, seed) {
+  var
+    l = str.length,
+    h = seed ^ l,
+    i = 0,
+    k;
+  
+  while (l >= 4) {
+  	k = 
+  	  ((str.charCodeAt(i) & 0xff)) |
+  	  ((str.charCodeAt(++i) & 0xff) << 8) |
+  	  ((str.charCodeAt(++i) & 0xff) << 16) |
+  	  ((str.charCodeAt(++i) & 0xff) << 24);
     
-    ascii += '\x80' // Append Ƈ' bit (plus zero padding)
-    while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
-    for (i = 0; i < ascii[lengthProperty]; i++) {
-        j = ascii.charCodeAt(i);
-        if (j>>8) return; // ASCII check: only accept characters in range 0-255
-        words[i>>2] |= j << ((3 - i)%4)*8;
-    }
-    words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
-    words[words[lengthProperty]] = (asciiBitLength)
-    
-    // process each chunk
-    for (j = 0; j < words[lengthProperty];) {
-        var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
-        var oldHash = hash;
-        // This is now the undefinedworking hash", often labelled as variables a...g
-        // (we have to truncate as well, otherwise extra entries at the end accumulate
-        hash = hash.slice(0, 8);
-        
-        for (i = 0; i < 64; i++) {
-            var i2 = i + j;
-            // Expand the message into 64 words
-            // Used below if 
-            var w15 = w[i - 15], w2 = w[i - 2];
+    k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+    k ^= k >>> 24;
+    k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
 
-            // Iterate
-            var a = hash[0], e = hash[4];
-            var temp1 = hash[7]
-                + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
-                + ((e&hash[5])^((~e)&hash[6])) // ch
-                + k[i]
-                // Expand the message schedule if needed
-                + (w[i] = (i < 16) ? w[i] : (
-                        w[i - 16]
-                        + (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
-                        + w[i - 7]
-                        + (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
-                    )|0
-                );
-            // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
-            var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
-                + ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
-            
-            hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
-            hash[4] = (hash[4] + temp1)|0;
-        }
-        
-        for (i = 0; i < 8; i++) {
-            hash[i] = (hash[i] + oldHash[i])|0;
-        }
-    }
-    
-    for (i = 0; i < 8; i++) {
-        for (j = 3; j + 1; j--) {
-            var b = (hash[i]>>(j*8))&255;
-            result += ((b < 16) ? 0 : '') + b.toString(16);
-        }
-    }
-    return result;
-};
+	h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)) ^ k;
+
+    l -= 4;
+    ++i;
+  }
+  
+  switch (l) {
+  case 3: h ^= (str.charCodeAt(i + 2) & 0xff) << 16;
+  case 2: h ^= (str.charCodeAt(i + 1) & 0xff) << 8;
+  case 1: h ^= (str.charCodeAt(i) & 0xff);
+          h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+  }
+
+  h ^= h >>> 13;
+  h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+  h ^= h >>> 15;
+
+  return h >>> 0;
+}
