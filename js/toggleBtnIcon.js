b229c3c20def10cb492be3ce388908d05cc1aa35
Phillip Abramson <Phillip.Abramson@monash.edu>
2021-02-18 10:16:50 +1100

    indenting for readability

--- js/toggleBtnIcon.js
+++ js/toggleBtnIcon.js
@@ -1,62 +1,62 @@
 window.onload=function(){
-if ($('#beestRead i').hasClass('fa-plus')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-plus fa-minus'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-plus-circle')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-plus-circle fa-minus-circle'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-plus-square')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-plus-square fa-minus-square'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-plus-square-o')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-plus-square-o fa-minus-square-o'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-arrow-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-arrow-down fa-arrow-up'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-arrow-circle-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-arrow-circle-down fa-arrow-circle-up'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-arrow-circle-o-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-arrow-circle-o-down fa-arrow-circle-o-up'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-chevron-circle-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-chevron-circle-down fa-chevron-circle-up');
-});
-}
-else if ($('#beestRead i').hasClass('fa-chevron-circle-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-chevron-circle-down fa-chevron-circle-up'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-caret-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-caret-down fa-caret-up'); 
-});
-}
-else if ($('#beestRead i').hasClass('fa-caret-square-o-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-caret-square-o-down fa-caret-square-o-up');
-});
-}
-else if ($('#beestRead i').hasClass('fa-hand-o-down')){
-    $('div #beestRead').click(function(){ 
-    $(this).find('i').toggleClass('fa-hand-o-down fa-hand-o-up'); 
-});
-}
+	if ($('#beestRead i').hasClass('fa-plus')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-plus fa-minus'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-plus-circle')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-plus-circle fa-minus-circle'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-plus-square')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-plus-square fa-minus-square'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-plus-square-o')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-plus-square-o fa-minus-square-o'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-arrow-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-arrow-down fa-arrow-up'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-arrow-circle-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-arrow-circle-down fa-arrow-circle-up'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-arrow-circle-o-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-arrow-circle-o-down fa-arrow-circle-o-up'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-chevron-circle-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-chevron-circle-down fa-chevron-circle-up');
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-chevron-circle-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-chevron-circle-down fa-chevron-circle-up'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-caret-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-caret-down fa-caret-up'); 
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-caret-square-o-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-caret-square-o-down fa-caret-square-o-up');
+	});
+	}
+	else if ($('#beestRead i').hasClass('fa-hand-o-down')){
+		$('div #beestRead').click(function(){ 
+		$(this).find('i').toggleClass('fa-hand-o-down fa-hand-o-up'); 
+	});
+	}
 }
